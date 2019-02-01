01a860429271c59916e2aeebe55b45051f33737a
Stephen Soltesz <soltesz@google.com>
2019-02-01 15:29:44 -0500

    Work better

--- cmd/geoloc-trial/main.go
+++ cmd/geoloc-trial/main.go
@@ -7,10 +7,12 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/m-lab/go/flagx"
-	"github.com/m-lab/go/rtx"
 )
 
 var (
@@ -21,6 +23,8 @@ var (
 func init() {
 	flag.StringVar(&url, "url", "https://mlab-ns.appspot.com/ndt_ssl?ip_address=", "URL prefix used to request")
 	flag.Var(&input, "file", "File to read contents.")
+
+	log.SetOutput(os.Stderr)
 }
 
 func main() {
@@ -30,19 +34,29 @@ func main() {
 	r := csv.NewReader(buf)
 
 	for rec, err := r.Read(); err == nil; rec, err = r.Read() {
-		ip := rec[0]
-		site := rec[1]
+		// ts := rec[0]
+		ip := rec[1]
+		site := rec[2]
 
 		resp, err := http.Get(url + ip)
-		rtx.Must(err, "Failed to get url for: %s", url)
+		if err != nil {
+			log.Printf("Failed to get url for: %s %s", url, err)
+			continue
+		}
 
 		b, err := ioutil.ReadAll(resp.Body)
-		rtx.Must(err, "Failed to read response body")
+		if err != nil {
+			log.Printf("Failed to read response body: %s", err)
+			continue
+		}
 
 		m := map[string]interface{}{}
 		err = json.Unmarshal(b, &m)
-		rtx.Must(err, "Failed to unmarshal response: %s", string(b))
+		if err != nil {
+			log.Printf("Failed to unmarshal response: %s : %s", string(b), err)
+			continue
+		}
 
-		fmt.Printf("%s appengine:%s maxmind:%s\n", ip, site[:3], m["site"].(string)[:3])
+		fmt.Printf("%s,%s,%s,%s\n", time.Now().Format(time.RFC3339), site[:3], m["site"].(string)[:3], ip)
 	}
 }
