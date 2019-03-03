ade8db1b56a535842001c0f823c1fee1c8c7102b
Stephen Soltesz <stephen.soltesz@gmail.com>
2019-03-02 22:31:21 -0500

    add build.sh add color support

--- community/build.sh
+++ community/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-set -x
+set -xe
 
 gopherjs build worldmap.go
-cat dscc.min.js sites.js worldmap.js worldMap.js > viz.js
+cat dscc.min.js palette.js tinycolor.js sites.js worldmap.js worldMap.js > viz.js
 gsutil -h 'Cache-Control:private, max-age=0, no-transform' cp -a public-read \
     viz.js viz.json viz.css manifest.json gs://soltesz-mlab-sandbox/v2/
 
