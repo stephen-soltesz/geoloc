6fea664d547527b481610f7be11e9cf4875c9dfc
Stephen Soltesz <stephen.soltesz@gmail.com>
2019-03-05 22:39:50 -0500

    Debug setup

--- community/worldMap.js
+++ community/worldMap.js
@@ -1,8 +1,8 @@
 // Create and add the canvas.
 
-// function drawWM(data) {
-//   loadData(null, data);
-// }
+function drawWM(data) {
+    console.log(JSON.stringify(data));
+}
 
 // subscribe to data and style changes.
-// dscc.subscribeToData(drawWM, {transform: dscc.objectTransform});
+dscc.subscribeToData(drawWM, {transform: dscc.objectTransform});
